e5e0b356c7127f78f45f6ac4f927a202a0f5f57d
ThienTimothy-Nguyen <minhthien2729@gmail.com>
2026-05-29 21:43:33 -0700

    create animation for home page

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 1000,
+});
 
 const LandingIntro = () => {
   return (
     <section id="section-intro" className="no-top no-bottom">
       <div className="container">
         <div className="row">
-          <div className="col-lg-4 col-md-6 mb-sm-30">
+          <div 
+            className="col-lg-4 col-md-6 mb-sm-30"
+            data-aos="zoom-out"
+            data-aos-duration="650"
+          >
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_wallet"></i>
+              <i 
+                className="bg-color-2 i-boxed icon_wallet"
+                data-aos="fade-up"
+                data-aos-delay="150"
+              ></i>
               <div className="text">
                 <h4 className="">Set up your wallet</h4>
-                <p>
+                <p
+                  data-aos="fade-up"
+                  data-aos-delay="400"
+                >
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
@@ -18,12 +34,23 @@ const LandingIntro = () => {
               <i className="wm icon_wallet"></i>
             </div>
           </div>
-          <div className="col-lg-4 col-md-6 mb-sm-30">
+          <div 
+            className="col-lg-4 col-md-6 mb-sm-30"
+            data-aos="zoom-out"
+            data-aos-duration="650"
+          >
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
+              <i 
+                className="bg-color-2 i-boxed icon_cloud-upload_alt"
+                data-aos="fade-up"
+                data-aos-delay="150"
+              ></i>
               <div className="text">
                 <h4 className="">Add your NFT's</h4>
-                <p>
+                <p
+                  data-aos="fade-up"
+                  data-aos-delay="400"
+                >
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
@@ -31,12 +58,23 @@ const LandingIntro = () => {
               <i className="wm icon_cloud-upload_alt"></i>
             </div>
           </div>
-          <div className="col-lg-4 col-md-6 mb-sm-30">
+          <div 
+            className="col-lg-4 col-md-6 mb-sm-30"
+            data-aos="zoom-out"
+            data-aos-duration="650"
+          >
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_tags_alt"></i>
+              <i 
+                className="bg-color-2 i-boxed icon_tags_alt"
+                data-aos="fade-up"
+                data-aos-delay="150"
+              ></i>
               <div className="text">
                 <h4 className="">Sell your NFT's</h4>
-                <p>
+                <p
+                  data-aos="fade-up"
+                  data-aos-delay="400"
+                >
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
